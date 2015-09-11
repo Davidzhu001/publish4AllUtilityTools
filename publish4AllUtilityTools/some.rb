a5dc1aa1118d16f5a1c827229f3b798131724d78
Davidzhu001 <qluoluo@icloud.com>
2015-09-11 16:27:55 -0500

    printer bootstrap contains

--- publish4AllUtilityTools/some.rb
+++ publish4AllUtilityTools/some.rb
@@ -10,3 +10,22 @@ xmldoc = Document.new(xmlfile)
 print "#{document_file}"
 # Now get the root element
 root = xmldoc.root
+
+
+
+
+
+# ruby validation of a url
+require 'net/http'
+
+def working_url?(url_str)
+    url = URI.parse(url_str)
+    Net::HTTP.start(url.host, url.port) do |http|
+        http.head(url.request_uri).code == '200'
+        p "true"
+    end
+    rescue
+    false
+    p "something"
+end
+working_url?("http://google",)
