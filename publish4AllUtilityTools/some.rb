45703ca5858d9a942e8074c781bc8a6da3774d53
Davidzhu001 <qluoluo@icloud.com>
2015-09-10 23:01:31 -0500

    some ruby in swift

--- publish4AllUtilityTools/some.rb
+++ publish4AllUtilityTools/some.rb
@@ -1,8 +1,10 @@
 require 'rexml/document'
 require 'open-uri'
+require 'json'
 include REXML
-
+hash =
 document_file = open("http://192.168.2.102/DevMgmt/ProductUsageDyn.xml"){ |f| f.read }
+something = Hash.from_xml(document_file).to_json
 xmlfile = File.new(document_file)
 xmldoc = Document.new(xmlfile)
 print "#{document_file}"
