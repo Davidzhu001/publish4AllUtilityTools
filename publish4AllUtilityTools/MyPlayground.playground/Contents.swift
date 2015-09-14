1db046dc52b3b5c7000079ae4e56e8dfcbc6ce15
Davidzhu001 <qluoluo@icloud.com>
2015-09-14 16:28:06 -0500

    adding structs

--- publish4AllUtilityTools/MyPlayground.playground/Contents.swift
+++ publish4AllUtilityTools/MyPlayground.playground/Contents.swift
@@ -1,19 +1,32 @@
-//: Playground - noun: a place where people can play
+////: Playground - noun: a place where people can play
+//
+//import Cocoa
+//
+//var parser = NSXMLParser()
+//var posts = NSMutableArray()
+//var elements = NSMutableDictionary()
+//var element = NSString()
+//var title1 = NSMutableString()
+//var date = NSMutableString()
+//
+//func beginParsing()
+//{
+//    posts = []
+//    parser = NSXMLParser(contentsOfURL: (NSURL (fileURLWithPath:"http://images.apple.com/main/rss/hotnews/hotnews.rss")))!
+//    parser.delegate = self
+//    parser.parse()
+//    tbData!.reloadData()
+//}
 
-import Cocoa
-
-var parser = NSXMLParser()
-var posts = NSMutableArray()
-var elements = NSMutableDictionary()
-var element = NSString()
-var title1 = NSMutableString()
-var date = NSMutableString()
-
-func beginParsing()
+struct Number
 {
-    posts = []
-    parser = NSXMLParser(contentsOfURL: (NSURL (fileURLWithPath:"http://images.apple.com/main/rss/hotnews/hotnews.rss")))!
-    parser.delegate = self
-    parser.parse()
-    tbData!.reloadData()
+    var digits: Int
+    let numbers = 3.1415
 }
+
+var n = Number(digits: 12345)
+n.digits = 67
+
+println("\(n.digits)")
+println("\(n.numbers)")
+n.numbers
---
+++ publish4AllUtilityTools/PrinterDataResources.swift
@@ -0,0 +1,13 @@
+//
+//  PrinterDataResources.swift
+//  publish4AllUtilityTools
+//
+//  Created by ZhuWeicheng on 9/14/15.
+//  Copyright (c) 2015 Zhu,Weicheng. All rights reserved.
+//
+
+import Foundation
+
+struct PrinterData {
+    
+}
