9e7d0afbe93ec21b47610f994e5ba46a538d309b
Davidzhu001 <qluoluo@icloud.com>
2015-09-14 14:56:45 -0500

    adding the table view in

--- publish4AllUtilityTools/ViewController.swift
+++ publish4AllUtilityTools/ViewController.swift
@@ -9,9 +9,11 @@
 import Cocoa
 import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet weak var webViewer: WebView!
+    @IBOutlet weak var tableView: NSTableView!
+    var objects:NSMutableArray! = NSMutableArray()
     
     
     override func viewDidLoad() {
@@ -21,11 +23,49 @@ class ViewController: NSViewController {
         let fragUrl = NSURL(string: "index.html", relativeToURL: try6)!
         let request = NSURLRequest(URL: fragUrl)
         self.webViewer.mainFrame.loadRequest(request)
+        
+        // table
+        
+        self.objects.addObject("something")
+        self.objects.addObject("something")
+        self.objects.addObject("something")
+        
+        self.tableView.reloadData()
     }
+    
+    
+    
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    
+    
+    
+    
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int
+    {
+        return self.objects.count
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+      var cellView = tableView.makeViewWithIdentifier("cell", owner: self) as! NSTableCellView
+        cellView.textField!.stringValue = self.objects.objectAtIndex(row) as! String
+        
+        return cellView
+     }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        if (self.tableView.numberOfSelectedRows > 0)
+        {
+            let selectedItem = self.objects.objectAtIndex(self.tableView.selectedRow) as! String
+            
+            println(selectedItem)
+            
+            self.tableView.deselectRow(self.tableView.selectedRow)
         }
     }
 }
