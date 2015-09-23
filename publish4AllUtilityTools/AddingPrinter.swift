d13f4b3991a02335569dda0af989e67af3eb3097
Davidzhu001 <qluoluo@icloud.com>
2015-09-23 11:21:09 -0500

    new window

---
+++ publish4AllUtilityTools/AddingPrinter.swift
@@ -0,0 +1,18 @@
+//
+//  AddingPrinter.swift
+//  publish4AllUtilityTools
+//
+//  Created by ZhuWeicheng on 9/23/15.
+//  Copyright © 2015 Zhu,Weicheng. All rights reserved.
+//
+
+import Cocoa
+
+class AddingPrinter: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
